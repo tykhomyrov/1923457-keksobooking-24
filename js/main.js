c06322abec6059ba27788c6e46026125ff6e5734
Vladyslav <tykhomyrov@ya.ru>
2021-11-04 20:48:03 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,14 @@
-let expectedUsers = 1000;
-
-let usersByDay = [817, 1370, 752, 1247, 681, 1120, 915, 1281, 875, 1341, 757, 610, 812, 1170, 769, 1261, 845, 1289, 515, 1247, 845, 1311, 741, 1239, 812, 638, 877, 1242, 1159, 1372];
-
-// Рисуем график посещаемости
-keks.plot(usersByDay, expectedUsers);
-
-// Суммируем посещаемость и анализируем провалы
-let totalUsers = 0;
-let minUsers = expectedUsers - 100;
-let badDays = [];
-
-for (let i = 0; i <= usersByDay.length - 1; i++) {
-  totalUsers += usersByDay[i];
-  if (usersByDay[i] < minUsers) {
-    badDays[i] = expectedUsers - usersByDay[i];
-  } else {
-    badDays[i] = 0;
-  }
+let str = prompt("Введите строоку " , "");
+let maxlegth = +prompt("Введите максимальную длинну " , "")
+if(str == undefined || str == ""){
+    alert("Строка не введена");
+}else{
+    truncate();
 }
-
-keks.plot(badDays, 100);
-
-// Рассчитываем среднее значение посещаемости
-let averageUsers = totalUsers / usersByDay.length;
-console.log('Средняя посещаемость: ' + averageUsers);
-
-if (averageUsers > expectedUsers) {
-  console.log('Посещаемость великолепна. Продолжай в том же духе!');
-} else {
-  console.log('Посещаемость так себе. Нужно поднапрячься!');
+function truncate() {
+    if (str.length >= maxlegth) {
+        return alert(str.slice(0 , maxlegth-1) + "...");
+    }else{
+        return alert(false);
+    }
 }
