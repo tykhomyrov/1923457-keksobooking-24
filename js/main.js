ea7c69d83629cb2ad4fcdb6d8350ff26e3ae4ad3
Vladyslav <tykhomyrov@ya.ru>
2021-11-04 20:24:48 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
-function getRandomArbitrary(min, max) 
-let maxDigits = 0;
-{
-    if (min > max || min < 0 || max <= 0) {
-      return ("Заданы неверные значения. Введите другие значения");
-    }
+let expectedUsers = 1000;
 
-    const digitsDegree = 10 ** maxDigits;
-    return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+let usersByDay = [817, 1370, 752, 1247, 681, 1120, 915, 1281, 875, 1341, 757, 610, 812, 1170, 769, 1261, 845, 1289, 515, 1247, 845, 1311, 741, 1239, 812, 638, 877, 1242, 1159, 1372];
+
+// Рисуем график посещаемости
+keks.plot(usersByDay, expectedUsers);
+
+// Суммируем посещаемость и анализируем провалы
+let totalUsers = 0;
+let minUsers = expectedUsers - 100;
+let badDays = [];
+
+for (let i = 0; i <= usersByDay.length - 1; i++) {
+  totalUsers += usersByDay[i];
+  if (usersByDay[i] < minUsers) {
+    badDays[i] = expectedUsers - usersByDay[i];
+  } else {
+    badDays[i] = 0;
   }
-  getRandomArbitrary(1, 2, 6);
+}
+
+keks.plot(badDays, 100);
+
+// Рассчитываем среднее значение посещаемости
+let averageUsers = totalUsers / usersByDay.length;
+console.log('Средняя посещаемость: ' + averageUsers);
+
+if (averageUsers > expectedUsers) {
+  console.log('Посещаемость великолепна. Продолжай в том же духе!');
+} else {
+  console.log('Посещаемость так себе. Нужно поднапрячься!');
+}
