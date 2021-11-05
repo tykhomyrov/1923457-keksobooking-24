67e4a44a8df8fd148e7268865c8cd21ec2ad98b4
Vladyslav <91688236+tykhomyrov@users.noreply.github.com>
2021-11-05 14:46:11 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
-let str = prompt("Введите строоку " , "");
-let maxlegth = +prompt("Введите максимальную длинну " , "")
-if(str == undefined || str == ""){
-    alert("Строка не введена");
-}else{
-    truncate();
-}
-function truncate() {
-    if (str.length >= maxlegth) {
-        return alert(str.slice(0 , maxlegth-1) + "...");
-    }else{
-        return alert(false);
+function getRandomArbitrary(min, max, maxDigits = 0) {
+    if (min > max || min < 0 || max <= 0) {
+      return ('Задан неверный диапазон! Укажите другие числа.');
     }
-}
+    const digitsDegree = 10 ** maxDigits;
+    return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+  }
+  getRandomArbitrary(1, 2, 6);
