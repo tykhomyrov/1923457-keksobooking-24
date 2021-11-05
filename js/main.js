b8303b776ce8f9fc8f30f42dd2b3d990219be38b
Vladyslav <tykhomyrov@ya.ru>
2021-11-05 16:57:18 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ function getRandomArbitrary(min, max, maxDigits = 0) {
   }
 
   const digitsDegree = 10 ** maxDigits;
-return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+  return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
 }
 getRandomArbitrary(1, 2, 6);
