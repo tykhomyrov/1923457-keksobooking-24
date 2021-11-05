53f78c14d9d3f5216efc1d2686e3606cbf53de9d
Vladyslav <tykhomyrov@ya.ru>
2021-11-05 16:11:45 +0400

    ja

--- js/main.js
+++ js/main.js
@@ -1,9 +1,2 @@
-function getRandomArbitrary(min, max, maxDigits=0) {
-    if (min > max || min < 0 || max <= 0) {
-        return ('Задан неверный диапазон! Укажите другие числа.');
-    }
+function getRandomArbitrary(min, max, maxDigits=0) {}
 
-    const digitsDegree = 10 ** maxDigits;
-        return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
-  }
-  getRandomArbitrary(1, 2, 6);
