b9aff09bf30e7c60a4a39228b62d3f607ad9432e
Vladyslav <tykhomyrov@ya.ru>
2021-11-05 15:37:00 +0400

    Create str.js

---
+++ js/str.js
@@ -0,0 +1,14 @@
+let str = prompt("Введите строоку ", "");
+let maxlegth = +prompt("Введите максимальную длинну ", "");
+if (str == undefined || str == "") {
+    alert("Строка не введена");
+} else {
+    truncate();
+}
+function truncate() {
+    if (str.length >= maxlegth) {
+        return alert(str.slice(0, maxlegth - 1) + "...");
+    } else {
+        return alert(false);
+    }
+}
